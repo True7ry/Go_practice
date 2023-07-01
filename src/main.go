ec2b7b5ac5109bc9e6af0cdbe0d965d87272367b
Truetry <calderonmikel16@gmail.com>
2023-06-30 22:34:05 -0300

    primer_contacto

---
+++ src/main.go
@@ -0,0 +1,11 @@
+package main // Esto es para nombre de la carpeta donde esta guardado.
+
+import "fmt"
+
+/*
+Como ejecutar un archivo?: go run
+Ejecutarlo para produccion(crea un archivo): go build src/main.go
+*/
+func main(){
+	fmt.Println("Terminal")
+}
